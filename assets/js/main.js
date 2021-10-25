1d8bb0fb980a9bc2e6e10d55ff67461cb296e0f0
King <kingsjacobfrancis@gmail.com>
2021-10-25 23:17:23 +0100

    changes made

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+const btnsubmit = document.querySelector("#sub_btn")
+btnsubmit.addEventListener('click', (e)=>{
+    e.preventDefault()
+    submitform()
+})
+function submitform(){
+    const myform = document.querySelector("#myform");
+    var fname = document.querySelector("#fname").value
+    var lname = document.querySelector("#lname").value
+    var email = document.querySelector("#email").value
+    var msg = document.querySelector("#msg").value
+
+    if (fname == "" || lname == "" || email == "" || msg == "") {
+        Swal.fire({
+            title: 'Error',
+            icon: 'error',
+            text: 'All fields are required!',
+            showClass: {
+              popup: 'animate__animated animate__bounceInRight'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__bounceOutLeft'
+            }
+          })
+    }
+    else{
+        myform.submit()
+        Swal.fire({
+            title: 'Success',
+            icon: 'success',
+            text: 'Message sent successfully!',
+            showClass: {
+              popup: 'animate__animated animate__bounceInRight'
+            },
+            hideClass: {
+              popup: 'animate__animated animate__bounceOutLeft'
+            }
+          })
+    }
+}
